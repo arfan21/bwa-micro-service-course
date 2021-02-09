f4f3d3b9fe4615637b958ef22f9e3ee21456763f
arfan21 <arfan.jadul10@gmail.com>
2021-02-09 17:29:13 +0700

    add dockerfile

--- app/helpers.php
+++ app/helpers.php
@@ -7,9 +7,9 @@ function getUser($userId)
     $url = env("SERVICE_USER_URL") . "users/" . $userId;
 
     try {
-        $repsonse = Http::timeout(10)->get($url);
-        $data = $repsonse->json();
-        $data["http_code"] = $repsonse->getStatusCode();
+        $response = Http::timeout(10)->get($url);
+        $data = $response->json();
+        $data["http_code"] = $response->getStatusCode();
         return ($data);
     } catch (\Throwable $th) {
         return [
@@ -44,4 +44,21 @@ function getUserByIds($userIds = [])
             "message" => "service user unavailable"
         ];
     }
+}
+
+function postOrder($params){
+    $url = env("SERVICE_ORDER_PAYMENT_URL") . "/api/orders";
+    try {
+        $response = Http::post($url, $params);
+        $data = $response->json();
+        $data["http_code"] = $response->getStatusCode();
+        return $data;
+    } catch (\Throwable $th) {
+        return [
+            "status" => "error",
+            "http_code" => 500,
+            "message" => "service order payment unavailable"
+        ];
+    }
+
 }
